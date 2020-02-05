8c237e59aff7bf041aca5a43c923d566b8a883e4
untilDawn <48229283+Babdodook@users.noreply.github.com>
2020-02-05 17:04:17 +0900

    Update TarkovBot.py

--- TarkovBot.py
+++ TarkovBot.py
@@ -10,7 +10,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.name)
     print("ready")
-    game = discord.Game("-")
+    game = discord.Game("킬라소환")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
